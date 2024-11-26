b859f0220877d73517dfa663a046a472c1b3cdfd
Juls0026 <julianru0026@gmail.com>
2024-11-26 17:46:51 +0100

    Improved UDP

--- Online_Tracks/Online_Tracks.cpp
+++ Online_Tracks/Online_Tracks.cpp
@@ -69,8 +69,10 @@ bool ALSAset(snd_pcm_t* &captureman, snd_pcm_t* &playbackman) {
 }
 
 
-//Sending audio function
+//Audio capture and send function
 void RecAndSend(snd_pcm_t* captureman, std::vector<sockaddr_in>& peerIP, int sockfd, std::atomic<bool>& running, std::mutex& peer_mute) {
+    
+    //Initialize
     Audio_Packet packet = {};
     uint32_t sequence = 0; 
 
@@ -104,7 +106,175 @@ void RecAndSend(snd_pcm_t* captureman, std::vector<sockaddr_in>& peerIP, int soc
     }
 }
 
+//Receive and playback audio function
+void AudioPlayback(snd_pcm_t* playbackman, int sockfd, std::atomic<bool>& runnning){
+
+    //Initialize
+    Audio_Packet packet = {};
+    uint32_t last_sequ = 0;
+    uint16_t silence[Buff_Size * Channels] = {0}; //Buffer of 0s for lost packets 
+
+    while (runnning) { 
+        ssize_t Audio_rec = recv(sockfd, &packet, sizeof(packet), 0);
+        if (Audio_rec < 0) {
+            std::cerr << "Error receiving audio. \n";
+            continue;
+        }
+
+
+        int Frames_r = snd_pcm_writei(playbackman, packet.audio_data, Buff_Size);
+        if (Frames_r < 0) {
+            snd_pcm_prepare(playbackman);
+        }
+
+        //Update sequence
+        last_sequ = packet.sequence;
+    }
+
+}
+
+
+//UDP hello broadcast 
+void sendHELLO (int sockfd, sockaddr_in& boradcast_ad, std::atomic<bool>& running) {
+    //Initialize 
+    const char* message = "HELLO"; 
+
+    //repetedly send message to constantly check peers
+    while (running) {
+        if (sendto(sockfd, message, strlen(message), 0, (struct sockaddr*)&boradcast_ad, sizeof(boradcast_ad)) < 0) {
+
+            std::cerr << "Failed to broadcast message " << strerror(errno) << "\n";      
+        }
+
+        //Wait to send message again 
+        std::this_thread::sleep_for(std::chrono::seconds(Broadcast_Int));
+
+    }
+}
+
+
+//UDP Find peers function 
+void LookForPeers(int sockfd, std::vector<sockaddr_in>& peer_List, std::mutex& peer_mute, std::atomic<bool>& running) {
+
+     //Initialize
+     char buffer[1024];
+     sockaddr_in peer_addr{};
+     socklen_t addr_l = sizeof(peer_addr);
+    
+
+    while (running) {
+        //Get message
+        ssize_t Audio_rec = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0, (struct sockaddr*)&peer_addr, &addr_l);
+        if (Audio_rec > 0) {
+            buffer[Audio_rec] = '\0';
+            if (strcmp(buffer, "HELLO") == 0) {
+                std::lock_guard<std::mutex> lock(peer_mute);
+                auto it = std::find_if(peer_List.begin(), peer_List.end(), [&peer_addr](const sockaddr_in& addr) {
+                    return addr.sin_addr.s_addr == peer_addr.sin_addr.s_addr && addr.sin_port == peer_addr.sin_port;
+                });
+                
+                //Add Peer to list
+                if (it == peer_List.end()) {
+                    peer_List.push_back(peer_addr);
+                    std::cout << "Peer: " <<inet_ntoa(peer_addr.sin_addr) << "\n";
+                }
+                }
+
+            }
+
+        }
+    }
 
 
 
+int main() {
 
+    //Initialize coommon variables 
+    std::vector<sockaddr_in> peer_List; //List of discovered peers
+    std::mutex peer_mute;               //Protecting peer List 
+    std::atomic<bool> running(true);    //Flag to control threads
+
+    //UDP scoket init 
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd <0) {
+        std::cerr << "Failed socket init: " << strerror(errno) << "\n";
+        return 1;
+    }
+
+    //Enable broadcasting 
+    int enable_brd = 1; 
+    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &enable_brd, sizeof(enable_brd))< 0) {
+        std::cerr << "Failed to initialize broadcast: " << strerror(errno) << "ºn";
+        close (sockfd);
+        return 1;
+    }
+
+    //Set socket address and port
+    sockaddr_in local_address = {};
+    local_address.sin_family = AF_INET;
+    local_address.sin_port = htons(UDP_port);
+    local_address.sin_addr.s_addr = INADDR_BROADCAST;
+
+    if (bind(sockfd, (struct sockaddr*)&local_address, sizeof(local_address)) < 0) {
+        std::cerr << "Socket bind failed: " <<strerror(errno) << "\n";
+        close(sockfd);
+        return 1;
+
+    }
+
+
+    //Set Broadcast address
+    sockaddr_in brd_address = {};
+    brd_address.sin_family = AF_INET;
+    brd_address.sin_port = htons(UDP_port);
+    brd_address.sin_addr.s_addr = INADDR_BROADCAST;
+
+
+    //Init ALSA 
+    snd_pcm_t* captureman = nullptr;
+    snd_pcm_t* playbackman = nullptr;
+
+
+    if(!ALSAset(captureman, playbackman)) {
+        std::cerr << "Failed to initialize ALSA. \n";
+        close(sockfd);
+        return 1;
+
+    }
+
+
+    //Start threads
+    std::thread broadcast(sendHELLO, sockfd, std::ref(brd_address), std::ref(running));
+    std::thread listen(LookForPeers, sockfd, std::ref(peer_List), std::ref(peer_mute), std::ref(running));
+    std::thread send_audio(RecAndSend, captureman, std::ref(peer_List), sockfd, std::ref(running), std::ref(peer_mute));
+    std::thread play_audio(AudioPlayback, playbackman, sockfd, std::ref(running));
+
+
+    //End program
+    std::cout << "Press Enter to stop the program... \n";
+    std::cin.get();
+
+    //stop threads
+    running = false; 
+
+    //join threads 
+    broadcast.join();
+    listen.join();
+    send_audio.join();
+    play_audio.join();
+
+
+
+    //clean up buffer
+    snd_pcm_close(captureman);
+    snd_pcm_close(playbackman);
+    close(sockfd);
+
+
+
+    std::cout << "End of program. \n";
+    return 0;
+    
+
+
+}
